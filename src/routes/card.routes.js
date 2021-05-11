e206f4c9fbff1e26a01933d8630995aadc0ee1a9
Shehan Thamel <shehan@ceylonsolutions.com>
2021-05-12 01:55:06 +0530

    API validations

--- src/routes/card.routes.js
+++ src/routes/card.routes.js
@@ -4,8 +4,11 @@ const {
     createCatCard,
     downloadCard
 } = require("../controllers/card.controller");
+const {
+    validateCreateCatCard
+} = require('../validators/card.validator');
 
-router.get('/', createCatCard);
+router.get('/', validateCreateCatCard, createCatCard);
 router.get('/:filename', downloadCard);
 
 module.exports = router;
---
+++ src/validators/card.validator.js
@@ -0,0 +1,58 @@
+const { checkSchema, validationResult } = require('express-validator');
+
+const errorFormatter = ({ msg }) => {
+    return msg;
+};
+
+module.exports.validateCreateCatCard = async (req, res, next) => {
+    try {
+        await Promise.all(
+            checkSchema({
+                greeting: {
+                    in: ['query'],
+                    optional: { options: { nullable: false } }
+                },
+                who: {
+                    in: ['query'],
+                    optional: { options: { nullable: false } }
+                },
+                width: {
+                    in: ['query'],
+                    optional: { options: { nullable: false } },
+                    isInt: {
+                        errorMessage: 'Width should be numeric'
+                    }
+                },
+                height: {
+                    in: ['query'],
+                    optional: { options: { nullable: false } },
+                    isInt: {
+                        errorMessage: 'Height should be numeric'
+                    }
+                },
+                color: {
+                    in: ['query'],
+                    optional: { options: { nullable: false } }
+                },
+                size: {
+                    in: ['query'],
+                    optional: { options: { nullable: false } },
+                    isInt: {
+                        errorMessage: 'Size should be numeric'
+                    }
+                }
+            }).map(validation => validation.run(req))
+        );
+
+        validationResult(req)
+            .formatWith(errorFormatter)
+            .throw();
+        next();
+    } catch (error) {
+        return res.status(422).json({ 
+            message: error.mapped && error.mapped()
+        });
+    }
+};
+
+
